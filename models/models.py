8f16c14e600368eec0e29fae99bf9deecfcd119f
iijimal825 <luna.iijima@gmail.com>
2021-02-28 00:15:10 +0900

    :art: fix #4: add email validation by regex.

--- models/models.py
+++ models/models.py
@@ -10,7 +10,7 @@ class MemberContent(Base):
     #__table_args__ = (UniqueConstraint('name','email'),{})
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True)
-    email = Column(String(120), unique=True)
+    email = Column(String(257), unique=True)
     birthday = Column(DateTime)
 
     def __init__(self, name=None, email=None, birthday=None):
